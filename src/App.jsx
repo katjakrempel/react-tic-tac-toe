2883d2973a82713195c248ccd66fb3a61d5d8839
Katja Krempel <katjakrempel@Katjas-Air.default>
2024-06-27 15:57:48 +0100

    complete Square and Board components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Board from "./components/Board";
+import Square from "./components/Square";
 
 function App() {
   return (
     <>
       <h1>Tic Tac Toe</h1>
+      <Board />
     </>
   );
 }
