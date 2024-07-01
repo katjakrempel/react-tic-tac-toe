b4a42b4b039112e3f5840d6300668bf41f960e09
Katja Krempel <katjakrempel@Katjas-Air.default>
2024-07-01 13:12:31 +0100

    conditional rendering for game history

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -19,14 +19,21 @@ function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
-    if (move > 0) {
+    if (move === currentMove && move > 0) {
+      description = "You are at move #" + move;
+    } else if (move > 0) {
       description = "Go to move #" + move;
     } else {
       description = "Go to game start";
     }
+
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {move === currentMove && move > 0 ? (
+          description
+        ) : (
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        )}
       </li>
     );
   });
